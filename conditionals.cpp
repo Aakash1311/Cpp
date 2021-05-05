fc3310720d6aa383ca497bdc185a53722b4dfaf4
Aakash1311 <Singhaakash084@gmail.com>
2021-05-05 10:34:38 +0530

    My First cpp code

---
+++ conditionals.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if (a>10 && a<20){
+        cout<<"Hello World!"<<endl;
+
+    } 
+    else if (a>=20 && a<30) {
+        cout<<"Big Bang"<<endl;
+
+        
+    }
+    else{
+        cout<<"Welcome to the World of Progrmming"<<endl;
+    }
+    return 0;
+}
